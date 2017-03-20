24a3e105e298bc79a69b93badbdfb76c856d5100
Camila Alvarez <cam.alvarez.i@gmail.com>
2017-03-20 11:17:57 -0300

    Modified AddCaffe.cmake so caffe it is downloaded and built before descriptor manager

--- cmake/AddCaffe.cmake
+++ cmake/AddCaffe.cmake
@@ -2,9 +2,9 @@ find_package(Threads REQUIRED)
 include(ExternalProject)
 ExternalProject_Add(Caffe
         URL https://github.com/BVLC/caffe/archive/rc5.zip
-        CMAKE_ARGS -DCPU_ONLY=ON
-        INSTALL_COMMAND "")
+        CMAKE_ARGS -DCPU_ONLY=ON -DOpenCV_DIR=/home/calvarez/source/OrandLibs/linux_64bits/opencv-3.1.0/build/
+        SOURCE_DIR ${CMAKE_SOURCE_DIR}/caffe
+        BINARY_DIR ${CMAKE_SOURCE_DIR}/caffe)
 
-ExternalProject_Get_Property(Caffe BINARY_DIR)
+find_package(Caffe REQUIRED HINTS ${CMAKE_SOURCE_DIR}/caffe)
 
-find_package(Caffe REQUIRED)
